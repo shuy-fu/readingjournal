6bc672612850bd6c82da859541a91f9b27ef2886
unknown <shu.y.fu@helsinki.fi>
2025-09-19 19:27:56 +0300

    Add new book form

--- app.py
+++ app.py
@@ -12,6 +12,23 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_book")
+def new_book():
+    return render_template("new_book.html")
+
+@app.route("/create_book", methods=["POST"])
+def create_book():
+    title = request.form["title"]
+    description = request.form["description"]
+    rating = request.form["rating"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO books (title, description, rating, user_id)
+            VALUES (?, ?, ?, ?)"""
+    db.execute(sql, [title, description, rating, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -22,16 +39,16 @@ def create():
     password1 = request.form["password1"]
     password2 = request.form["password2"]
     if password1 != password2:
-        return "VIRHE: salasanat eivät ole samat"
+        return "ERROR: passwords do not match"
     password_hash = generate_password_hash(password1)
 
     try:
         sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
         db.execute(sql, [username, password_hash])
     except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
+        return "ERROR: username is taken"
 
-    return "Tunnus luotu"
+    return "Account created"
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -42,16 +59,20 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
-            return "VIRHE: väärä tunnus tai salasana"
+            return "ERROR: invalid username or password"
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    rating INTEGER,
+    user_id INTEGER REFERENCES users
+);
