80538c962cf6b33f497fc567311b22874f076adf
unknown <shu.y.fu@helsinki.fi>
2025-10-13 13:50:02 +0300

    fix registration error

--- db.py
+++ db.py
@@ -9,10 +9,15 @@ def get_connection():
 
 def execute(sql, params=[]):
     con = get_connection()
-    result = con.execute(sql, params)
-    con.commit()
-    g.last_insert_id = result.lastrowid
-    con.close()
+    try:
+        result = con.execute(sql, params)
+        con.commit()
+        g.last_insert_id = result.lastrowid
+    except Exception as e:
+        con.rollback()
+        raise e
+    finally:
+        con.close()
 
 def last_insert_id():
     return g.last_insert_id    
